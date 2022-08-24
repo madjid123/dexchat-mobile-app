cd6a228e0b8f9b609c2c02e8316ee06870d46b46
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-08-24 19:47:47 +0100

    change  styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import "CustomWidget.dart";
 
 void main() {
   runApp(MyApp());
@@ -12,18 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          //primaryColor: Color(0xFF362929)
-          ),
+      theme: ThemeData(primaryColor: Color(0xFF362929)),
       home: MyHomePage(title: 'dexchat Hello'),
     );
   }
@@ -31,15 +21,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -52,34 +33,22 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        backgroundColor: Color.fromARGB(255, 20, 100, 100),
-        title: Center(
-          child: Text("kdjsf"),
-        ),
+        backgroundColor: Color.fromARGB(0, 97, 97, 97),
+        title: Center(child: CustomWidget()),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(30))),
+        actions: [],
       ),
 
-      backgroundColor: Color.fromARGB(255, 40, 40, 40),
+      backgroundColor: Color.fromARGB(255, 29, 29, 31),
 
       body: Center(
           // Center is a layout widget. It takes a single child and positions it
@@ -91,17 +60,17 @@ class _MyHomePageState extends State<MyHomePage> {
               children: <Widget>[
                 Text('You have pushed the button this many times:',
                     style: GoogleFonts.poppins(
-                        fontWeight: FontWeight.bold,
+                        fontWeight: FontWeight.normal,
                         fontSize: 15,
-                        color: Colors.white)),
+                        color: Color.fromARGB(255, 202, 202, 202))),
                 Text('$_counter',
-                    style:
-                        GoogleFonts.poppins(color: Colors.white, fontSize: 40)),
+                    style: GoogleFonts.poppins(
+                        color: Color(0xFFE4E4E4), fontSize: 40)),
                 Container(
                     padding: EdgeInsets.all(50.0),
                     margin: EdgeInsets.all(50),
                     child: TextField(
-                      style: TextStyle(color: Colors.white),
+                      style: TextStyle(color: Color(0xFFE4E4E4)),
                       decoration: InputDecoration(
                         border: InputBorder.none,
                         alignLabelWithHint: true,
@@ -118,9 +87,7 @@ class _MyHomePageState extends State<MyHomePage> {
           )),
       drawer: Drawer(
         child: ListView(
-          // Important: Remove any padding from the ListView.
           padding: EdgeInsets.zero,
-
           children: [
             const DrawerHeader(
               decoration:
