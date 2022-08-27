0e00dbca5423dfc48159791d45916575f211d39e
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-08-27 16:27:54 +0100

    explore some styling

--- lib/main.dart
+++ lib/main.dart
@@ -48,62 +48,84 @@ class _MyHomePageState extends State<MyHomePage> {
       //   actions: [],
       // ),
 
-      backgroundColor: Color.fromARGB(255, 101, 101, 109),
+      backgroundColor: Color.fromARGB(255, 34, 34, 37),
 
       body: Align(
           alignment: Alignment.topCenter,
-          child: Container(
-            margin: EdgeInsets.fromLTRB(20, 100, 20, 20),
-            padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(40),
-                border: Border.all(
-                  color: Color.fromARGB(255, 31, 32, 33),
-                  width: 3.0,
-                )),
-            child: Column(
-              children: <Widget>[
-                Text('You have pushed the button this many times:',
-                    style: GoogleFonts.poppins(
-                        fontWeight: FontWeight.normal,
-                        fontSize: 15,
-                        color: Color.fromARGB(255, 202, 202, 202))),
-                Text('$_counter',
-                    style: GoogleFonts.poppins(
+          child: Column(children: <Widget>[
+            Container(
+              margin: EdgeInsets.fromLTRB(10, 60, 10, 10),
+              padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Color.fromARGB(38, 98, 98, 98)),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Text(
+                      "hello World",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Text(
+                      "hello World",
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ]),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+              padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(40),
+                  border: Border.all(
+                    color: Color.fromARGB(255, 31, 32, 33),
+                    width: 3.0,
+                  )),
+              child: Column(
+                children: <Widget>[
+                  Text('Login',
+                      style: GoogleFonts.poppins(
                         color: Color(0xFFE4E4E4),
                         fontSize: 40,
-                        fontWeight: FontWeight.bold)),
-                Container(
-                    padding: EdgeInsets.all(10),
-                    child: TextField(
-                      style: TextStyle(color: Color(0xFFE4E4E4)),
-                      decoration: InputDecoration(
-                        fillColor: Colors.cyanAccent,
-                        focusColor: Colors.deepOrange,
-                        prefixIconColor: Colors.black,
-                        focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                                width: 2.0, color: Colors.deepOrange)),
-                        border: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Color.fromARGB(255, 207, 64, 64),
-                              width: 4.0,
-                            ),
-                            borderRadius: BorderRadius.all(Radius.circular(20)),
-                            gapPadding: 4),
-                        alignLabelWithHint: true,
-                        labelText: "Name",
-                        labelStyle: TextStyle(color: Colors.deepOrange),
-                        hintText: "Enter your name",
-                        hintStyle: TextStyle(color: Colors.grey[500]),
-                      ),
-                      onChanged: (value) {
-                        print(value);
-                      },
-                    ))
-              ],
-            ),
-          )),
+                        fontWeight: FontWeight.bold,
+                      )),
+                  Container(
+                      padding: EdgeInsets.all(10),
+                      child: TextField(
+                        style: TextStyle(color: Color(0xFFE4E4E4)),
+                        decoration: InputDecoration(
+                          focusColor: Colors.deepOrange,
+
+                          prefixIconColor: Colors.white,
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.deepOrange),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20))),
+                          border: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.deepOrange),
+                              borderRadius: BorderRadius.circular(20)),
+                          // border: OutlineInputBorder(
+                          //     borderSide: BorderSide(
+                          //       color: Color.fromARGB(255, 207, 64, 64),
+                          //       width: 4.0,
+                          //     ),
+                          // borderRadius:
+                          //     BorderRadius.all(Radius.circular(20)),
+                          // gapPadding: 4),
+                          alignLabelWithHint: true,
+                          labelText: "Name",
+                          labelStyle: TextStyle(color: Colors.deepOrange),
+                          hintText: "Enter your name",
+                          hintStyle: TextStyle(color: Colors.grey[500]),
+                        ),
+                        onChanged: (value) {
+                          print(value);
+                        },
+                      ))
+                ],
+              ),
+            )
+          ])),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
