baa727aa76dbbbd68c2f3a589416c1c3ee85d3ba
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-08-27 21:53:13 +0100

    styling ...

--- lib/CustomWidget.dart
+++ lib/CustomWidget.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CustomWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("DexChat"),
-    );
+        child:
+            Text("DexChat", style: GoogleFonts.poppins(color: Colors.white54)));
   }
 }
