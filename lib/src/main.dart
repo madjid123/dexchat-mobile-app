6e10f86b32783d886378440e97f8b8660aee6302
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-09-04 20:33:05 +0100

    fix customWidget name

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'CustomWidget.dart';
+import './widgets/CustomWidget.dart';
 
 void main() {
   runApp(MyApp());
